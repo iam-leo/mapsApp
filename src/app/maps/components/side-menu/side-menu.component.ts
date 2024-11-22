164a14081c1f5e3ef92f2bbc2165c830321f7923
Leo Ss <leo.vm.cba@gmail.com>
2024-11-22 10:40:30 -0300

    ✅ add: sidebar menu

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface MenuItem {
+  route: string;
+  name: string;
+  icon: string;
+}
 
 @Component({
-  selector: 'app-side-menu',
+  selector: 'map-side-menu',
   templateUrl: './side-menu.component.html',
   styles: ``
 })
 export class SideMenuComponent {
+  public menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name: 'Fullscreen', icon: 'assets/icons/fullscreen.svg' },
+    { route: '/maps/zoom-range', name: 'Zoom', icon: 'assets/icons/zoom.svg' },
+    { route: '/maps/markers', name: 'Markers', icon: 'assets/icons/marker.svg' },
+    { route: '/maps/properties', name: 'Houses', icon: 'assets/icons/house.svg' },
+  ]
 
+  constructor(public router: Router) {}
 }
