17a52ca2fcabafc12bfa04f95c4e8dc2fbea7b35
Leo Ss <leo.vm.cba@gmail.com>
2024-11-24 10:35:31 -0300

    ✅ add: render initial map

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as tt from '@tomtom-international/web-sdk-maps';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styles: ``
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = tt.map({
+      key: environment.TOMTOM_KEY, // Usar API Key
+      container: 'map', // ID del elemento contenedor
+      center: [-64.183, -31.417], // Coordenadas iniciales [longitud, latitud]
+      zoom: 5 // Nivel inicial de zoom
+    });
+
+    // Agregar controles de zoom
+    map.addControl(new tt.NavigationControl());
+  }
 
 }
