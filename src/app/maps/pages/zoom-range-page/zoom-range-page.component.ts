38809a85b006e0b278230ecfe0c06a40bb0726b7
Leo Ss <leo.vm.cba@gmail.com>
2024-11-30 16:42:07 -0300

    ✅ add: zoom control buttons for zoomRagePage

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { map, NavigationControl } from '@tomtom-international/web-sdk-maps';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styles: ``
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+  @ViewChild('map') public divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap ) return
+
+    const zoomMap = map({
+      key: environment.TOMTOM_KEY, // Usar API Key
+      container: this.divMap?.nativeElement, // ID del elemento contenedor
+      center: [-64.183, -31.417], // Coordenadas iniciales [longitud, latitud]
+      zoom: 5 // Nivel inicial de zoom
+    });
+
+    // Agregar controles de zoom
+    zoomMap.addControl(new NavigationControl());
+  }
 
 }
