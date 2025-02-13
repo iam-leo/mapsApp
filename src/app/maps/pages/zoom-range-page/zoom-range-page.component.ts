b93ea4488899429b371e0d00939e234c50a47ce5
Leo Ss <leo.vm.cba@gmail.com>
2025-02-13 20:45:18 -0300

    ✅ add: zoom controls for buttons

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { map, NavigationControl } from '@tomtom-international/web-sdk-maps';
+import { Map, map, NavigationControl } from '@tomtom-international/web-sdk-maps';
 import { environment } from '../../../../environments/environment';
 
 @Component({
@@ -9,18 +9,57 @@ import { environment } from '../../../../environments/environment';
 export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild('map') public divMap?: ElementRef;
 
+  public levelZoom: number = 10;
+  public map?: Map;
+
   ngAfterViewInit(): void {
     if ( !this.divMap ) return
 
-    const zoomMap = map({
+    this.map = map({
       key: environment.TOMTOM_KEY, // Usar API Key
       container: this.divMap?.nativeElement, // ID del elemento contenedor
       center: [-64.183, -31.417], // Coordenadas iniciales [longitud, latitud]
-      zoom: 5 // Nivel inicial de zoom
+      zoom: this.levelZoom // Nivel inicial de zoom
     });
 
     // Agregar controles de zoom
-    zoomMap.addControl(new NavigationControl());
+    this.map.addControl(new NavigationControl());
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if ( !this.map ) throw new Error('Mapa no inicializado');
+
+    this.map.on('zoom', () => {
+      this.levelZoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', () => {
+      if( this.map!.getZoom() < 18) return;
+
+      this.map!.setZoom(18);
+    });
+  }
+
+  zoomIn() {
+    if ( !this.map ) throw new Error('Mapa no inicializado');
+
+    const currentZoom = this.map.getZoom();
+    this.map.setZoom(currentZoom + 1);
+  }
+
+  zoomOut() {
+    if ( !this.map ) throw new Error('Mapa no inicializado');
+
+    const currentZoom = this.map.getZoom();
+    this.map.setZoom(currentZoom - 1);
+  }
+
+  changeZoom( value: string ) {
+    if ( !this.map ) throw new Error('Mapa no inicializado');
+
+    this.map.setZoom( +value );
   }
 
 }
