9a9d677ad9f30b552f74a237b88afffc3c4f34c5
Leo Ss <leo.vm.cba@gmail.com>
2025-03-11 21:05:24 -0300

    ✅ add: map card for houses

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, map, Map, Marker} from '@tomtom-international/web-sdk-maps';
+import { environment } from '../../../../environments/environment';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'mini-map',
   templateUrl: './mini-map.component.html',
   styles: ``
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
+  @ViewChild('map', { static: false }) public divMap?: ElementRef;
+  @Input() lngLat?: [number, number] = [0,0];
+
+  public map?: Map;
+
+
+  ngAfterViewInit(): void {
+   if (!this.divMap) {
+      console.error('Map container is not defined.');
+      return;
+    }
+    if (!this.lngLat || this.lngLat.length !== 2 || this.lngLat.some(coord => coord == null)) {
+      console.error(`Invalid lngLat: ${JSON.stringify(this.lngLat)}`);
+      return;
+    }
+
+    // create a map
+    this.map = map({
+      key: environment.TOMTOM_KEY, // Usar API Key
+      container: this.divMap?.nativeElement, // ID del elemento contenedor
+      center: this.lngLat, // Coordenadas iniciales [longitud, latitud]
+      zoom: 15, // Nivel inicial de zoom
+      interactive: false, // Desactivar interacción
+    });
+
+    // Agregar un marker
+    this.map.on('load', () => {
+      if(!this.lngLat) throw new Error('lngLat is not defined');
+      const marker = new Marker({ anchor: 'center', color: 'rgb(55, 48, 163)' })
+        .setLngLat(this.lngLat)
+        .addTo(this.map!);
+    });
+
+  }
 
 }
--- src/app/maps/pages/properties-page/properties-page.component.ts
+++ src/app/maps/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 interface House {
     title: string;
@@ -10,19 +10,19 @@ interface House {
   templateUrl: './properties-page.component.html',
   styles: ``
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnInit {
   public houses: House[] = [
       {
         title: 'Casa residencial, Canadá',
         description: 'Bella propiedad en Katana, Canadá',
         lngLat: [ -75.92722289474008, 45.280015511264466]
       },
-      {
+       {
         title: 'Casa de playa, México',
         description: 'Hermosa casa de playa en Acapulco, México',
         lngLat: [ -99.91287720907991, 16.828940930185748]
       },
-      {
+      /*{
         title: 'Apartamento, Argentina',
         description: 'Lujoso apartamento en el corazón de Buenos Aires, Argentina',
         lngLat: [ -58.430166677283445, -34.57150108832866 ]
@@ -31,7 +31,15 @@ export class PropertiesPageComponent {
         title: 'Local comercial, España',
         description: 'Local comercial disponible en Madrid, España, cerca de El Jardín Secreto.',
         lngLat: [ -3.7112735618380177, 40.42567285425766 ]
-      },
+      }, */
     ];
+
+    public selectedHouse: House | null = null;
+
+
+
+    ngOnInit(): void {
+      console.log(this.houses);
+    }
 
 }
