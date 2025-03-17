da12cf23b40c4e4af52b47902ce9bc978585fbc5
Leo Ss <leo.vm.cba@gmail.com>
2025-03-17 19:47:57 -0300

    ✅ add: first standalone component

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alone-page',
+  standalone: true,
+  imports: [ CommonModule],
+  templateUrl: './alone-page.component.html',
+  styles: ``
+})
+export class AlonePageComponent {
+
+}
